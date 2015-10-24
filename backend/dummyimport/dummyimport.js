718df278a68077ababc8fe35e72710f8a0c41c7d
Tuukka Ojala <tuukka.ojala@gmail.com>
2015-10-24 20:45:58 +0300

    Get the dummy importer back.

---
+++ backend/dummyimport/dummyimport.js
@@ -0,0 +1,25 @@
+var mongodb = require("mongodb");
+var journeys = require("./journeys").body;
+
+var client = mongodb.MongoClient,
+    dburl = process.env.MONGODB_URI || 'mongodb://localhost/test';
+
+client.connect(dburl, function(err, db) {
+    if (err) {
+        throw err;
+    }
+    var stops = db.collection('stops');
+    journeys.filter(function(journey) {
+        return journey.headSign === 'Janka';
+    })
+    .forEach(function(j) {
+        j.calls.forEach(function(call) {
+            stops.insert({ time: call.arrivalTime, name: call.stopPoint.name, left: 0 }, function(err) {
+                if (err) {
+                    throw err;
+                }
+            });
+        });
+    });
+    db.close();
+});
